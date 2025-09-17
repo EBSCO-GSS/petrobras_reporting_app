55eddcca41694075ef0b9123b77011f372d9899f
Rui Francisco <blackmolly@gmail.com>
2025-09-17 15:00:21 +0100

    Added report metadata

--- queries/producao_novos_bibliograficos.sql
+++ queries/producao_novos_bibliograficos.sql
@@ -1,16 +1,32 @@
+DROP FUNCTION IF EXISTS producao_novos_bibliograficos;
+
+CREATE FUNCTION producao_novos_bibliograficos(
+    start_date date DEFAULT '2000-01-01',
+    end_date date DEFAULT '2050-01-01'
+)
+RETURNS TABLE(
+    Usuario text,
+    AnoMes text,
+    Total numeric)
+AS $$
 select 
     (u.jsonb->'personal'->>'firstName' || ' ' || (u.jsonb->'personal'->>'lastName')) AS patron,
     to_char(
 	    date_trunc('month', (i.jsonb->'metadata'->>'createdDate')::timestamp),
 	    'YYYY-MM'
 	) AS created_month,
-    (i.jsonb->'metadata'->>'createdByUserId') AS createdby,
     COUNT(*) AS total
 FROM folio_inventory.instance__ i
 LEFT JOIN folio_users.users__ u
        ON u.id = (i.jsonb->'metadata'->>'createdByUserId')::uuid
 GROUP BY
     created_month,
-    createdby,
     patron
-ORDER BY created_month DESC, patron;
+where (i.jsonb->'metadata'->>'createdDate')::timestamp between start_date and end_date
+ORDER BY created_month DESC, patron
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
+
+
