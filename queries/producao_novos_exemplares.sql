e3b8e34b0f03d551cb4ed2f0094e216bc81e3927
Rui Francisco <blackmolly@gmail.com>
2025-09-18 08:01:25 +0100

    Updated parameters to mandatory

--- queries/producao_novos_exemplares.sql
+++ queries/producao_novos_exemplares.sql
@@ -1,12 +1,12 @@
-DROP FUNCTION IF EXISTS reporting.producao_novos_exemplares;
+DROP FUNCTION IF EXISTS producao_novos_exemplares;
 
-CREATE FUNCTION reporting.producao_novos_exemplares(
+CREATE FUNCTION producao_novos_exemplares(
     start_date date DEFAULT '2000-01-01',
     end_date date DEFAULT '2050-01-01'
 )
 RETURNS TABLE(
     Usuario text,
-    AnoMes text,
+    Ano_Mes text,
     Total text)
 AS $$
 
@@ -15,16 +15,16 @@ select
     to_char(
 	    date_trunc('month', (i.jsonb->'metadata'->>'createdDate')::timestamp),
 	    'YYYY-MM'
-	) AS AnoMes,
+	) AS Ano_Mes,
     COUNT(*) AS Total
 FROM folio_inventory.item__ i
 LEFT JOIN folio_users.users__ u
        ON u.id = (i.jsonb->'metadata'->>'createdByUserId')::uuid
 where (i.jsonb->'metadata'->>'createdDate')::timestamp between start_date and end_date
 GROUP BY
-    AnoMes,
+    Ano_Mes,
     Usuario
-ORDER BY AnoMes DESC, Usuario;
+ORDER BY Ano_Mes DESC, Usuario;
 $$
 LANGUAGE SQL
 STABLE
