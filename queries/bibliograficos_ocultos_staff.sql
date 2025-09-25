52afffc86443e37c1726e84e3c246672b3c5fbfe
Rui Francisco <blackmolly@gmail.com>
2025-09-25 14:17:50 +0100

    Produção - Staff surpress

---
+++ queries/bibliograficos_ocultos_staff.sql
@@ -0,0 +1,33 @@
+--metadb:function producao_ocultos_staff
+
+DROP FUNCTION IF EXISTS producao_ocultos_staff;
+
+CREATE FUNCTION producao_ocultos_staff(
+    start_date date DEFAULT '2020-01-01',
+    end_date date DEFAULT '2050-01-01'
+)
+RETURNS TABLE(
+    Usuario text,
+    Ano_Mes text,
+    Total text)
+AS $$
+SELECT
+    (u.jsonb->'personal'->>'firstName' || ' ' || (u.jsonb->'personal'->>'lastName')) AS Usuario,
+    to_char(
+        date_trunc('month', (i.jsonb->'metadata'->>'updatedDate')::timestamp),
+        'YYYY-MM'
+    ) AS Ano_Mes,
+    COUNT(*) AS total
+FROM folio_inventory.instance__ i
+LEFT JOIN folio_users.users__ u
+       ON u.id = (i.jsonb->'metadata'->>'updatedByUserId')::uuid
+WHERE (i.jsonb->>'staffSuppress')::boolean =true
+and  (i.jsonb->'metadata'->>'updatedDate')::timestamp between start_date and end_date
+GROUP BY
+    Ano_Mes,
+    Usuario
+ORDER BY Ano_Mes DESC, Usuario
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
