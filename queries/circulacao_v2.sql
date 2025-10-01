e2e84b46524aea38e37276d6db391e1d4014e5e1
Rui Francisco <blackmolly@gmail.com>
2025-10-01 12:15:21 +0100

    Circulation v2

--- queries/circulacao_v2.sql
+++ queries/circulacao_v2.sql
@@ -5,8 +5,8 @@ DROP FUNCTION IF EXISTS circulacao_v2;
 CREATE FUNCTION circulacao_v2(
     start_date date DEFAULT '2020-01-01',
     end_date date DEFAULT '2050-01-01',
-    punidade_organizacional text DEFAULT ''
-    --plotacao  text DEFAULT '',
+    punidade_organizacional text DEFAULT '',
+    plotacao  text DEFAULT ''
     --pposicao  text DEFAULT ''
 )
 RETURNS TABLE(
@@ -50,7 +50,7 @@ LEFT JOIN folio_users.groups__t__ g
 WHERE i2.__current
   AND l.loan_date BETWEEN start_date AND end_date
   AND (u.jsonb->'customFields'->>'link')     LIKE '%' || punidade_organizacional || '%'
-  --AND (u.jsonb->'customFields'->>'stocking') LIKE '%' || plotacao || '%'
+  AND (u.jsonb->'customFields'->>'stocking') LIKE '%' || plotacao || '%'
   --AND (u.jsonb->'customFields'->>'position') LIKE '%' || pposicao || '%'
 GROUP BY 
     data_emprestimo, 
