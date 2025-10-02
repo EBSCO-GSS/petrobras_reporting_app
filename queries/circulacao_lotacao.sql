5ee237d4b7b1d9c35a214570d95ece20b60caf3c
Rui Francisco <blackmolly@gmail.com>
2025-10-02 16:24:57 +0100

    Update circulacao_lotacao.sql

--- queries/circulacao_lotacao.sql
+++ queries/circulacao_lotacao.sql
@@ -15,30 +15,33 @@ RETURNS TABLE(
     "Total empréstimos" bigint
 )
 AS $$
-SELECT distinct 
-	to_char(
-        date_trunc('month', (l.loan_date)),
-        'YYYY-MM'
-    ) AS data_emprestimo,
-	(u.jsonb->'customFields'->>'stocking') as  "Lotação",
-	count(*) as "Total empréstimos"
-
-from  folio_circulation.loan__t__ l 
-    LEFT JOIN folio_users.users__ u  ON u.id = l.user_id 
-    left join folio_inventory.item__t__ i2 on i2.id = l.item_id 
-    left join folio_inventory.holdings_record__t__ ht on ht.id= i2.holdings_record_id 
-    left join folio_inventory.instance__t__ i on i.id = ht.instance_id 
-    left join folio_inventory.service_point__t__ sp on sp.id =l.checkout_service_point_id 
-    left join folio_inventory.material_type__t__ mt on mt.id = i2.material_type_id
-
+SELECT
+    to_char(date_trunc('month', l.loan_date), 'YYYY-MM') AS data_emprestimo,
+    u.jsonb->'customFields'->>'stocking' AS "Lotação",
+    COUNT(*) AS "Total empréstimos"
+FROM folio_circulation.loan__t__ l 
+LEFT JOIN folio_users.users__ u  
+       ON u.id = l.user_id 
+LEFT JOIN folio_inventory.item__t__ i2 
+       ON i2.id = l.item_id 
+LEFT JOIN folio_inventory.holdings_record__t__ ht 
+       ON ht.id = i2.holdings_record_id 
+LEFT JOIN folio_inventory.instance__t__ i 
+       ON i.id = ht.instance_id 
+LEFT JOIN folio_inventory.service_point__t__ sp 
+       ON sp.id = l.checkout_service_point_id 
+LEFT JOIN folio_inventory.material_type__t__ mt 
+       ON mt.id = i2.material_type_id
 WHERE l.loan_date BETWEEN start_date AND end_date
   AND (u.jsonb->'customFields'->>'link')     LIKE '%' || punidade_organizacional || '%'
   AND (u.jsonb->'customFields'->>'stocking') LIKE '%' || plotacao || '%'
   AND (u.jsonb->'customFields'->>'position') LIKE '%' || pposicao || '%'
-  
-group by data_emprestimo, (u.jsonb->'customFields'->>'stocking')   
-ORDER BY data_emprestimo  desc, (u.jsonb->'customFields'->>'stocking') 
-
+GROUP BY 
+    to_char(date_trunc('month', l.loan_date), 'YYYY-MM'),
+    u.jsonb->'customFields'->>'stocking'
+ORDER BY 
+    data_emprestimo DESC, 
+    u.jsonb->'customFields'->>'stocking';
 $$
 LANGUAGE SQL
 STABLE
