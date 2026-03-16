ad69d98e95417d8403975d9c9ddb44998835dcfa
Rui Francisco <blackmolly@gmail.com>
2026-03-16 17:22:36 +0000

    Updates to rollup

--- queries/bibliograficos_ocultos_staff.sql
+++ queries/bibliograficos_ocultos_staff.sql
@@ -1,7 +1,5 @@
 --metadb:function producao_ocultos_staff
-
 DROP FUNCTION IF EXISTS producao_ocultos_staff;
-
 CREATE FUNCTION producao_ocultos_staff(
     start_date date DEFAULT '2020-01-01',
     end_date date DEFAULT '2050-01-01'
@@ -9,27 +7,27 @@ CREATE FUNCTION producao_ocultos_staff(
 RETURNS TABLE(
     Usuario text,
     Ano_Mes text,
-    Total text)
+    Total bigint)
 AS $$
 SELECT
+    CASE
+        WHEN GROUPING(
+            (u.jsonb->'personal'->>'firstName' || ' ' || (u.jsonb->'personal'->>'lastName'))
+        ) = 1
+        THEN 'GRAND TOTAL'
+        ELSE (u.jsonb->'personal'->>'firstName' || ' ' || (u.jsonb->'personal'->>'lastName'))
+    END AS Usuario,
     CASE
         WHEN GROUPING(to_char(
             date_trunc('month', (i.jsonb->'metadata'->>'updatedDate')::date),
             'YYYY-MM'
         )) = 1
-        THEN 'GRAND TOTAL'
+        THEN 'SUBTOTAL'
         ELSE to_char(
             date_trunc('month', (i.jsonb->'metadata'->>'updatedDate')::date),
             'YYYY-MM'
         )
     END AS Ano_Mes,
-    CASE
-        WHEN GROUPING(
-            (u.jsonb->'personal'->>'firstName' || ' ' || (u.jsonb->'personal'->>'lastName'))
-        ) = 1
-        THEN 'SUBTOTAL'
-        ELSE (u.jsonb->'personal'->>'firstName' || ' ' || (u.jsonb->'personal'->>'lastName'))
-    END AS Usuario,
     COUNT(DISTINCT i.id) AS total
 FROM folio_inventory.instance__ i
 LEFT JOIN folio_users.users__ u
@@ -38,18 +36,18 @@ WHERE (i.jsonb->>'staffSuppress')::boolean = true
   AND (i.jsonb->'metadata'->>'updatedDate')::date BETWEEN start_date AND end_date
   AND i.__current
 GROUP BY ROLLUP (
+    (u.jsonb->'personal'->>'firstName' || ' ' || (u.jsonb->'personal'->>'lastName')),
     to_char(
         date_trunc('month', (i.jsonb->'metadata'->>'updatedDate')::date),
         'YYYY-MM'
-    ),
-    (u.jsonb->'personal'->>'firstName' || ' ' || (u.jsonb->'personal'->>'lastName'))
+    )
 )
 ORDER BY
+    (u.jsonb->'personal'->>'firstName' || ' ' || (u.jsonb->'personal'->>'lastName')) NULLS LAST,
     to_char(
         date_trunc('month', (i.jsonb->'metadata'->>'updatedDate')::date),
         'YYYY-MM'
-    ) DESC NULLS LAST,
-    (u.jsonb->'personal'->>'firstName' || ' ' || (u.jsonb->'personal'->>'lastName')) NULLS LAST;
+    ) DESC NULLS LAST;
 $$
 LANGUAGE SQL
 STABLE
